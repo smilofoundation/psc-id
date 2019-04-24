c4d902f4079c42da536d860b535a4b9de70b042b
Thomas Modeneis <thomas.modeneis@gmail.com>
2019-04-24 16:25:35 +0200

    refactor webpack build and related

--- server.js
+++ server.js
@@ -4,24 +4,42 @@ const timeout = require('connect-timeout');
 
 var app = new (require('express'))()
 const PORT = parseInt(process.env.PORT || "3000");
+const isDEV = process.env.IS_DEV;
+
 
 //disable powered by
 app.disable('x-powered-by');
 
-app.use(serveStatic(path.join(__dirname, 'public')))
-
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, 'dist')));
 //set timeout for all requests 20s
 app.use(timeout(20000));
 
-app.use(function (req, res) {
-    res.sendFile(__dirname + '/src/index.html')
-})
+if(isDEV) {
+    console.log("==> 🌎  Starting webpack dev middleware ... ");
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const config = require('./webpack.config');
+    const compiler = webpack(config);
 
+    app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: '/'}));
+    app.use(webpackHotMiddleware(compiler));
+
+     app.use(function (req, res) {
+         res.redirect("/");
+    });
+
+} else {
+    app.use(function (req, res) {
+        res.sendFile(__dirname + '/dist/index.html');
+    });
+}
 
 app.listen(PORT, function (error) {
     if (error) {
-        console.error(error)
+        console.error(error);
     } else {
-        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", PORT, PORT)
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", PORT, PORT);
     }
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,75 @@
 var path = require('path')
 var webpack = require('webpack')
+const Fiber = require('fibers');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx'],
     },
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        path.join(__dirname, 'src', 'main.jsx')
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/'
+    entry: {
+        app: path.join(__dirname, 'src', 'main.jsx'),
     },
     plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development')
         }),
-        // new HtmlWebpackPlugin({
-        //     template: './src/index.html'
-        // })
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
     ],
     module: {
         rules: [{
             test: /\.(js|jsx)$/,
             loaders: ['babel-loader'],
             exclude: /node_modules/,
-            include: __dirname
-        }]
+            include: /src/
+        },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader",
+                    options: {
+                        implementation: require("sass"),
+                        fiber: Fiber
+                    }
+                }]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            maxSize: 3000000,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            }
+        }
     }
 }
 
